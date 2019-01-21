fc9d350ca0a49ac37683f17fb65780015e250511
Hector Ariceaga <hector_ariceaga@hotmail.com>
2019-01-21 17:03:00 +0000

    Automatically backed up by Learn

--- src/ColorSelector.js
+++ src/ColorSelector.js
@@ -3,7 +3,11 @@ import React, { Component } from 'react';
 export default class ColorSelector extends Component {
 
   handleClick = () => (
+<<<<<<< HEAD
 
+=======
+    
+>>>>>>> 8bd6474aa06813a9b8e44545ca990a075e7404da
   )
   makeColorSwatches = () => (
     ["#F00", "#F80", "#FF0", "#0F0", "#00F", "#508", "#90D", "#FFF", "#000"].map((str, idx) => {
